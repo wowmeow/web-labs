cea4c511c36624a345dcddca9f4f71ff0d2a55c4
orehov <orehov@zelax.ru>
2021-06-16 18:44:29 +0300

    Изменил базу данных

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-import path from 'path';
-import bodyParser from 'body-parser';
-import hbs from 'hbs';
-import mysql from 'mysql2';
-import bcrypt from 'bcrypt';
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const path = require('path');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const Person = require ('./models/person');
+
 
 const app = express();
-const PORT = 3000;
+const PORT = 7000;
 const saltRounds = 10;
 const _dirname = path.resolve();
 const Parser = bodyParser.urlencoded({
     extended: false
-});
-const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    database: "web_labs",
-    password: "root"
 });
 
 app.use(express.static(path.join(_dirname, '/public')));
@@ -30,7 +25,30 @@ app.get('/', (req, res) => {
         count: 200
     });
 });
-// app.get("/count", Parser, (req, res) =>{
+
+app.get("/count", Parser, (req, res) =>{
+    Person.find({},(err, result)=>{
+        if(err) console.log(err)
+        else {
+            res.send(JSON.stringify(result.length))
+        }
+    }) 
+});
+
+app.post('/registr', Parser, (req, res) => {
+        const person = new Person({
+            name: req.body.Name,
+            phone: req.body.Phone
+        })
+        person.save();
+        res.redirect('/');
+});
+// const app2 = express();
+
+// app2.get('/', (req, res) => {
+//     res.send('This second server');
+// });
+// app2.get("/count", Parser, (req, res) => {
 //     connection.query("SELECT COUNT(*) as count FROM person", function (err, results, fields) {
 //         let count_person = JSON.stringify(results[0].count);
 //         res.send(count_person);
@@ -38,48 +56,27 @@ app.get('/', (req, res) => {
 //     });
 // });
 
-app.post('/registr', Parser, (req, res) => {
-    connection.query("SELECT COUNT(*) as count FROM person", function (err, results, fields) {
-        let count_users = results[0].count;
-        // число записей
-        connection.query("INSERT INTO person VALUES(?, ?, ?)", [count_users++, req.body.Name,bcrypt.hashSync(req.body.Phone, saltRounds)], function (err, results) {
-            if (err) console.error(err);
-            else {
-                res.redirect('/');
-                console.log("Данные добавлены");
-            }
-        });
+// const jsonPlaceholderProxy = createProxyMiddleware({
+//     target: ' http://localhost:4000',
+//     changeOrigin: true,
+//     logLevel: 'debug',
+// });
+
+// app.use('/', jsonPlaceholderProxy);
+// app.use('/count', jsonPlaceholderProxy);
+
+
+
+// app2.listen(4000, () => {
+//     console.log(`Server start! PORT : 4000...`);
+// });
+
+async function start() {
+    const url = 'mongodb+srv://user:pB7yo8R84NlPjgPO@cluster0.1kobw.mongodb.net/YoGo';
+    await mongoose.connect(url, {useNewUrlParser: true});
+    app.listen(PORT, () => {
+        console.log(`Server start! PORT : ${PORT}...`);
     });
-});
-const app2 = express();
+}
 
-app2.get('/', (req, res) => {
-    res.send('This second server');
-});
-app2.get("/count", Parser, (req, res) => {
-    connection.query("SELECT COUNT(*) as count FROM person", function (err, results, fields) {
-        let count_person = JSON.stringify(results[0].count);
-        res.send(count_person);
-        // число записей
-    });
-});
-
-const jsonPlaceholderProxy = createProxyMiddleware({
-    target: ' http://localhost:4000',
-    changeOrigin: true,
-    logLevel: 'debug',
-});
-
-app.use('/', jsonPlaceholderProxy);
-app.use('/count', jsonPlaceholderProxy);
-
-
-
-app2.listen(4000, () => {
-    console.log(`Server start! PORT : 4000...`);
-});
-
-
-app.listen(PORT, () => {
-    console.log(`Server start! PORT : ${PORT}...`);
-});
+start();
